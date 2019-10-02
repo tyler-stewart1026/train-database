19106ec0f3cce20a27070e9cdb69847283ab4234
Tyler Stewart <tyler.stewart1026@gmail.com>
2019-10-01 19:01:41 -0600

    Did moment JS

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -11,9 +11,10 @@ var firebaseConfig = {
 
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
-
 // Global Variables
 var database = firebase.database();
+var now = moment();
+var minsAway = 0;
 // Click function set to the input values of the search bars
 $(document).on('click', '#addBtn', function (event) {
     event.preventDefault;
@@ -46,3 +47,59 @@ $(document).on('click', '#addBtn', function (event) {
     $('#firstTime').val('');
     $('#frequency').val('');
 });
+
+
+    database.ref().on("child_added", function(childSnapshot) {
+        console.log(childSnapshot.val());
+        
+        // store data into variables 
+        var trainName = childSnapshot.val().name;
+        var trainDestination = childSnapshot.val().destination;
+        var trainFirstTime = childSnapshot.val().firstTime;
+        var trainFrequency = childSnapshot.val().frequency;
+        
+        console.log(trainName);
+        console.log(trainDestination);
+        console.log(trainFirstTime);
+        console.log(trainFrequency);
+        
+        // Create the new row
+        var newRow = $("<tr>").append(
+           $("<td>").text(trainName),
+           $("<td>").text(trainDestination),
+           $("<td>").text(trainFirstTime),
+           $("<td>").text(trainFrequency),
+           $("<td>").text(tArrival),
+           $("<td>").text(tMin)
+         );
+
+
+         $(".trainTable > tbody").append(newRow);
+    
+
+    // Calculations using moment.js for "next arrival" and "minutes away"
+  var timeArr = trainFirstTime.split(":");
+  console.log(timeArr);
+  var trainTime = moment().hours(timeArr[0]).minutes(timeArr[1]);
+  console.log(trainTime);
+  var maxMoment = moment.max(moment(),trainTime);
+  console.log(maxMoment);
+  
+
+  if (maxMoment === trainTime) {
+    tArrival = trainTime.format("hh:mm A");
+    tMin = trainTime.diff(moment(), "minutes");
+  } else {
+    var difTime = moment().diff(trainTime, "minutes");
+    var tRemain = difTime % childSnapshot.val().frequency;
+    tMin = childSnapshot.val().frequency - tRemain;
+    tArrival = moment().add(tMin, "m").format("hh:mm A");
+  }
+  console.log(tArrival);
+  console.log(tMin);
+});
+
+
+
+
+
